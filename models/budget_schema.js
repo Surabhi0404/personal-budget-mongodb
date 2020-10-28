74780d34e3c319fe6a9c12b963698bc1d5b88071
Surabhi0404 <surabhigurav96@gmail.com>
2020-10-28 18:29:17 -0400

    Added POST and GET endpoints

---
+++ models/budget_schema.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const budgetSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+
+    },
+    value: {
+        type: Number,
+        trim: true,
+        required: true,
+    },
+    color: {
+        type: String,
+        minlength: 6,
+        validate: [colorValidator, 'not a valid color'],
+        required: true
+        
+    }
+}, {collection: 'budget'})
+
+function colorValidator (v) {
+    if (v.indexOf('#') == 0) {
+        if (v.length == 7) {  // #f0f0f0
+            return true;
+        }
+    }
+    return COLORS.indexOf(v) > -1;
+};
+
+module.exports = mongoose.model('budget', budgetSchema)
--- server.js
+++ server.js
@@ -2,13 +2,57 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose')
+const budgetModel = require("./models/budget_schema")
+const bodyParser = require("body-parser");
+
+let url = 'mongodb://localhost:27017/personal_budget_db';
 
 app.use(cors());
+app.use(bodyParser.json());
 
-const budget = require('./budgetdata.json')
+
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>{
+        budgetModel.find({})
+        .then((data)=>{
+            res.json(data);
+            mongoose.connection.close();
+        })
+        .catch((connectionError)=>{
+            console.log(connectionError);
+        })
+    })
+    .catch((connectionError)=>{
+        console.log(connectionError);
+    })
+});
+
+app.post('/budget/insert', (req, res) => {
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>{
+        console.log("Connected to the database");
+        let postData = new budgetModel({
+            title: req.body.title,
+            value: req.body.value,
+            color: req.body.color
+        });
+        budgetModel.insertMany(postData)
+            .then((data)=>{
+                res.json(data);
+                mongoose.connection.close();
+            })
+            .catch((connectionError)=>{
+                res.json(connectionError);
+                console.log(connectionError);
+            })
+    })
+    .catch((connectionError)=>{
+        console.log(connectionError);
+    })
+    
 });
 
 app.listen(port, () => {
